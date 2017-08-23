ab6d7938a9c1fd05470f262786666437258a75a9
Russ Cam <russ.cam@forloop.co.uk>
2017-08-24 09:10:51 +1000

    Add custom converter for PostJobDataRequest

--- src/Nest/XPack/MachineLearning/PostJobData/PostJobDataRequest.cs
+++ src/Nest/XPack/MachineLearning/PostJobData/PostJobDataRequest.cs
@@ -1,9 +1,14 @@
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Elasticsearch.Net;
 using Newtonsoft.Json;
 
 namespace Nest
 {
 	// TODO! Consider IPostJobDataRequest<T>
+	[JsonConverter(typeof(PostJobDataConverter))]
 	public partial interface IPostJobDataRequest
 	{
 		/// <summary>
@@ -29,5 +34,43 @@ namespace Nest
 
 		/// <inheritdoc />
 		public PostJobDataDescriptor Data(IEnumerable<object> data) => Assign(a => a.Data = data);
+
+		/// <inheritdoc />
+		public PostJobDataDescriptor Data(params object[] data) => Assign(a =>
+		{
+			if(data?.Length == 1 && data[0] is IEnumerable)
+			{
+				a.Data = ((IEnumerable)data[0]).Cast<object>();
+			}
+			else a.Data = data;
+		});
+	}
+
+	internal class PostJobDataConverter : JsonConverter
+	{
+		public override bool CanRead => false;
+
+		public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+		{
+			var request = (IPostJobDataRequest) value;
+			if (request == null)
+			{
+				writer.WriteNull();
+				return;
+			}
+
+			var settings = serializer.GetConnectionSettings();
+			var elasticsearchSerializer = settings.Serializer;
+			foreach (var data in request.Data)
+			{
+				var bodyJson = elasticsearchSerializer.SerializeToString(data, SerializationFormatting.None);
+				writer.WriteRaw(bodyJson + "\n");
+			}
+		}
+
+		public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer) =>
+			throw new NotSupportedException();
+
+		public override bool CanConvert(Type objectType) => true;
 	}
 }
