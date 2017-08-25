59ba8297423f1dece30304e180ded2c78567a8f6
codebrain <stuart.cam@elastic.co>
2017-08-25 11:23:58 +1000

    Fix reference

--- src/Tests/Framework/ManagedElasticsearch/Clusters/XPackMachineLearningCluster.cs
+++ src/Tests/Framework/ManagedElasticsearch/Clusters/XPackMachineLearningCluster.cs
@@ -4,6 +4,7 @@ using Nest;
 using Tests.Framework.Integration;
 using Tests.Framework.ManagedElasticsearch.NodeSeeders;
 using Tests.Framework.ManagedElasticsearch.Plugins;
+using Tests.Framework.ManagedElasticsearch.Tasks.InstallationTasks;
 
 namespace Tests.Framework.ManagedElasticsearch.Clusters
 {
