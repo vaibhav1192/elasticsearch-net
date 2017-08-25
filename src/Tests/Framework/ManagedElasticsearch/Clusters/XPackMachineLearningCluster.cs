61914a4d093e2ad00f0a57bd8b864dd13a958404
codebrain <stuart.cam@elastic.co>
2017-08-25 11:22:48 +1000

    New cluster type and download/unzip sample data task

---
+++ src/Tests/Framework/ManagedElasticsearch/Clusters/XPackMachineLearningCluster.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+using Nest;
+using Tests.Framework.Integration;
+using Tests.Framework.ManagedElasticsearch.NodeSeeders;
+using Tests.Framework.ManagedElasticsearch.Plugins;
+
+namespace Tests.Framework.ManagedElasticsearch.Clusters
+{
+	[RequiresPlugin(ElasticsearchPlugin.XPack)]
+	public class XPackMachineLearningCluster : ClusterBase
+	{
+		protected string[] XPackSettings => TestClient.VersionUnderTestSatisfiedBy(">=5.5.0")
+			? new[] {"xpack.security.authc.token.enabled=true"}
+			: new string[] {} ;
+
+		protected override string[] AdditionalServerSettings => base.AdditionalServerSettings.Concat(this.XPackSettings).ToArray();
+
+		public override ConnectionSettings ClusterConnectionSettings(ConnectionSettings s) =>
+			s.BasicAuthentication("es_admin", "es_admin");
+
+		protected override void SeedNode() => new DefaultSeeder(this.Node).SeedNode();
+
+		protected override InstallationTaskBase[] AdditionalInstallationTasks => new InstallationTaskBase[] { new DownloadMachineLearningSampleDataDistribution() };
+	}
+}
---
+++ src/Tests/Framework/ManagedElasticsearch/Tasks/InstallationTasks/DownloadMachineLearningSampleDataDistribution.cs
@@ -0,0 +1,21 @@
+﻿using Tests.Framework.ManagedElasticsearch.Nodes;
+
+namespace Tests.Framework.ManagedElasticsearch.Tasks.InstallationTasks
+{
+	public class DownloadMachineLearningSampleDataDistribution : InstallationTaskBase
+	{
+		public override void Run(NodeConfiguration config, NodeFileSystem fileSystem)
+		{
+			var from = "https://download.elasticsearch.org/demos/machine_learning/gettingstarted/server_metrics.tar.gz";
+			var to = Path.Combine(config.FileSystem.RoamingFolder, "server_metrics.tar.gz");
+			if (File.Exists(to)) return;
+
+			Console.WriteLine($"Download machine learning sample data from: {from}");
+			this.DownloadFile(from, to);
+			Console.WriteLine($"Downloaded machine learning sample data to: {to}");
+
+			Console.WriteLine($"Unzipping machine learning sample data: {from} ...");
+			ZipFile.ExtractToDirectory(to, fileSystem.RoamingFolder);
+		}
+	}
+}
