088d18c235e51e3eaa03895de85b26ab0df941e9
Russ Cam <russ.cam@elastic.co>
2017-10-06 13:54:16 +1100

    Stop datafeed before closing job

--- src/Tests/XPack/MachineLearning/StartDatafeed/StartDatafeedApiTests.cs
+++ src/Tests/XPack/MachineLearning/StartDatafeed/StartDatafeedApiTests.cs
@@ -26,6 +26,7 @@ namespace Tests.XPack.MachineLearning.StartDatafeed
 		{
 			foreach (var callUniqueValue in values)
 			{
+				StopDatafeed(client, callUniqueValue.Value);
 				CloseJob(client, callUniqueValue.Value);
 				DeleteJob(client, callUniqueValue.Value);
 			}
